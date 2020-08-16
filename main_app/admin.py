0d1bdd1deb20e5a92cfdafc333da777423926548
Sumedha <sumedhashetty8@gmail.com>
2020-08-16 12:44:04 +0530

    Database done

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Type,Form,Question,Option
+# Register your models here.
+
+admin.site.register(Type)
+admin.site.register(Form)
+admin.site.register(Question)
+admin.site.register(Option)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Type(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE , default=1)
+    role = models.CharField(max_length=100)
+
+class Form(models.Model):
+    name = models.CharField(max_length=100)
+    form_link = models.ManyToManyField(User)
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=100)
+    number = models.IntegerField(default=0)
+    form_link = models.ForeignKey(Form,on_delete=models.CASCADE,related_name='question',null=True,blank=True)
+    
+class Option(models.Model):
+    name = models.CharField(max_length=100)
+    question_link = models.ForeignKey(Question,on_delete=models.CASCADE,related_name='answer',null=True,blank=True)
